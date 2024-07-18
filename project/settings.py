2acc0f4ec1abad5b612cccc4d3b447834be078a5
emunconslting <emunconsultingltd@gmail.com>
2024-07-18 17:07:16 +0300

    Changes in ststicfiles_dir

--- project/settings.py
+++ project/settings.py
@@ -142,4 +142,4 @@ MEDIA_ROOT = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/static/')]
